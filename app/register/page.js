3be7a2f50499b1b426c1b4dd69f8fa5be5a76692
mahmoud hesham <83496901+muhmoud505@users.noreply.github.com>
2025-03-14 12:41:36 +0200

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -64,7 +64,7 @@ export default function Register() {
   
       if (data.success) {
         setIsRegistered(true);
-        router.redirect('/')
+        router.push('/')
       } else {
         setError(data.message || 'Registration failed');
       }
